4e25ca7a8586b912931e6e0ef3ce870c3e260cd5
Hakim Benyoucef <benyoucef.hakim@gmail.com>
2025-11-10 23:36:49 +0100

    fix : fix seed file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
 import PageTemplate from "@/components/templates/page-template";
-import { ticketPath } from "@/paths";
+import { ticketsPath } from "@/paths";
 
 export default function Home() {
   return (
     <PageTemplate title="Home" description="Welcome to the Ticketing System">
-      <Link href={ticketPath()} className="text-blue-500 underline">
+      <Link href={ticketsPath()} className="text-blue-500 underline">
         View Tickets
       </Link>
     </PageTemplate>
